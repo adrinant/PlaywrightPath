0d2eb226cd7aecbd9dc01de65eaaa91d407de3d4
adrinant <adrinant2000@gmail.com>
2024-05-23 04:30:27 +0700

    5th commit

---
+++ pages/home-page.ts
@@ -0,0 +1,26 @@
+import {type Locator, type Page, expect} from '@playwright/test';
+
+export class HomePage{
+    // variables
+    readonly page:Page;
+    // await page.getByRole('link', { name: 'Get started' }).click();
+    readonly getStartedButton: Locator;
+    //await expect(page).toHaveTitle(/Playwright/);
+    readonly pageTitle: RegExp;
+
+    //constructor
+    constructor (page: Page){
+        this.page = page;
+        this.getStartedButton = page.getByRole('link', { name: 'Get started' });
+        this.pageTitle = /Playwright/;
+    };
+
+    //method or functions
+    async clickGetStarted(){
+        await this.getStartedButton.click();
+    };
+
+    async assertPageTitle(){
+        await expect(this.page).toHaveTitle(this.pageTitle);
+    }
+}
